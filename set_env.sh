265be13517b65cb83ef825fb4299093cd3fa4e77
vomanhhung1995 <vomanhhung1995@gmail.com>
2024-03-20 21:38:51 +0700

    First commit after run source at local

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-v139-project03.ctk2zalctmyd.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3-269483980255-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=fpt
+export URL=http://localhost:8100
